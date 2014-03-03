5c533d4029f82c04ced2e0dfc37bc2b0a2b60202
Oscar Giraldo Castillo <oscar.gi.cast@gmail.com>
2014-03-02 21:58:46 -0500

    refactor simple code

--- main.py
+++ main.py
@@ -3,4 +3,15 @@ import os
 
 FPS_IN = 10
 FPS_OUT = 24
-os.system("avconv -r %s -i image%s.jpg -r %s -vcodec libx264 -crf 20 -g 15 -vf crop=500:500,scale=720:720 timelapse.mp4" % (FPS_IN, '%1d', FPS_OUT))
+FORMAT = '%1d'
+
+os.system(
+    "avconv -r %(FPS_IN)s -i img/image%(FORMAT)s.jpg -r %(FPS_OUT)s -vcodec"
+    " libx264 -crf 20 -g 15 -vf crop=640:480,scale=320:240"
+    " timelapse.mp4" %
+    {
+        'FPS_IN': FPS_IN,
+        'FPS_OUT': FPS_OUT,
+        'FORMAT': FORMAT,
+    }
+)
